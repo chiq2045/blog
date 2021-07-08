b0d4277dc6ad736940678fde200a98efc6dc927b
chiq2045 <charles.oroko@outlook.com>
2021-07-08 14:29:44 +0300

    update app

--- backend/app.ts
+++ backend/app.ts
@@ -1,20 +1,27 @@
-import Koa, { Context } from 'koa';
-import Router from '@koa/router';
+import Koa from 'koa';
 import logger from 'koa-logger';
-import { exceptionHandler } from './apps/middleware/exception-handler.middleware';
+import Lokijs from 'lokijs';
+import koaBody from 'koa-body';
+import { userRouter } from './apps/users';
+import { exceptionHandler } from './middleware/exception-handler.middleware';
+import { SINGLETON as UserDAO } from './apps/users/user.controller';
+import {authRouter} from './apps/auth';
 
 const app = new Koa();
-const router = new Router();
 
-router.get('/', async (ctx: Context) => {
-  ctx.body = 'OK';
-  ctx.status = 200;
-})
+const DB = new Lokijs('blog.json', {
+  autosave: true
+});
 
-app.use(logger())
+UserDAO.configure(DB);
+
+app.use(logger());
 app.use(exceptionHandler);
+app.use(koaBody());
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(userRouter.routes());
+app.use(userRouter.allowedMethods());
+app.use(authRouter.routes());
+app.use(authRouter.allowedMethods());
 
 export { app };
